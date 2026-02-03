cd291dd2bfcfa91d7008ca7f808cf2666badc6e6
sbx_user1051 <sbx_user1051@169.254.66.45>
2026-02-03 19:15:02 +0000

    Tue Feb 03 19:15:02 UTC 2026

--- lib/solutions/IWC/test_queue.py
+++ lib/solutions/IWC/test_queue.py
@@ -2,6 +2,42 @@ from solutions.IWC.queue_solution_legacy import Queue
 from solutions.IWC.task_types import TaskDispatch, TaskSubmission
 from datetime import datetime
 
+# 1. Enqueue: user_id=1, provider="bank_statements", timestamp='2025-10-20 12:00:00' -> 1 (queue size)
+# 2. Enqueue: user_id=1, provider="id_verification", timestamp='2025-10-20 12:01:00' -> 2 (queue size)  
+# 3. Enqueue: user_id=2, provider="companies_house", timestamp='2025-10-20 12:02:00' -> 3 (queue size)  
+# 4. Dequeue -> {"user_id": 1, "provider": "id_verification"}  
+# 5. Dequeue -> {"user_id": 2, "provider": "companies_house"}  
+# 6. Dequeue -> {"user_id": 1, "provider": "bank_statements"}  
+
+
+def test_bank_statements_deprio():
+    task1 = TaskSubmission(
+        user_id=1,
+        provider="bank_statements",
+        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task2 = TaskSubmission(
+        user_id=1,
+        provider="id_verification",
+        timestamp=datetime.strptime('2025-10-20 12:01:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task3 = TaskSubmission(
+        user_id=2,
+        provider="companies_house",
+        timestamp=datetime.strptime('2025-10-20 12:02:00', "%Y-%m-%d %H:%M:%S")
+    )
+
+    queue = Queue()
+    queue.enqueue(task1)
+    queue.enqueue(task2)
+    queue.enqueue(task3)
+
+    assert queue.dequeue() == TaskDispatch(provider="id_verification", user_id=1)
+    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=2)
+    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
+    assert queue.dequeue() == None
+
+
 
 
 def test_dedupe_credit_check():
@@ -142,3 +178,4 @@ def test_dependency_resolution():
 
     assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
     assert queue.dequeue() == TaskDispatch(provider="credit_check", user_id=1)
+
