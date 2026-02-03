26890bd70a24bcbe1ea4a7c59335626641d86dbe
sbx_user1051 <sbx_user1051@169.254.48.57>
2026-02-03 21:56:31 +0000

    Tue Feb 03 21:56:31 UTC 2026

--- lib/solutions/IWC/test_bank_statements.py
+++ lib/solutions/IWC/test_bank_statements.py
@@ -4,3 +4,13 @@ from datetime import datetime
 
 
 
+# 1. Enqueue: user_id=1, provider="id_verification", timestamp='2025-10-20 12:00:00' -> 1 (queue size)
+# 2. Enqueue: user_id=2, provider="bank_statements", timestamp='2025-10-20 12:01:00' -> 2 (queue size)
+# 3. Enqueue: user_id=3, provider="companies_house", timestamp='2025-10-20 12:07:00' -> 3 (queue size)
+# 4. Dequeue -> {"user_id": 1, "provider": "id_verification"}
+# 5. Dequeue -> {"user_id": 2, "provider": "bank_statements"}
+# 6. Dequeue -> {"user_id": 3, "provider": "companies_house"}
+
+
+def test_time_sesative_bank_statements():
+    
