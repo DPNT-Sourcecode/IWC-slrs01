b7fede8613f8345ad2a7d033966cb7514da7ac73
sbx_user1051 <sbx_user1051@169.254.48.57>
2026-02-03 21:50:07 +0000

    Tue Feb 03 21:50:07 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -188,21 +188,20 @@ class Queue:
         if len(self._queue) == 0 or len(self._queue) == 1:
             return 0
         
-        
-
-        queue_by_date = sorted(self._queue, key=attrgetter("timestamp"))
+        queue_by_date = sorted(self._queue, key=attrgetter("timestamp"), reverse=True)
 
         start_time = queue_by_date[0].timestamp
-        end_time = queue_by_date[len(queue_by_date)-1]
+        end_time = queue_by_date[len(queue_by_date)-1].timestamp
 
         if isinstance(start_time, str):
             start_time = datetime.strptime(queue_by_date[0].timestamp, "%Y-%m-%d %H:%M:%S")
+
         if isinstance(end_time, str):
-            start_time = datetime.strptime(queue_by_date[0].timestamp, "%Y-%m-%d %H:%M:%S")
+            end_time = datetime.strptime(queue_by_date[len(queue_by_date)-1].timestamp, "%Y-%m-%d %H:%M:%S")
 
-        time_diff = queue_by_date[0].timestamp - queue_by_date[len(queue_by_date)-1].timestamp
+        time_diff = start_time - end_time
 
-        return abs(time_diff.total_seconds())
+        return int(time_diff.total_seconds())
 
 
     def purge(self):
@@ -292,6 +291,7 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
+
 
 
 
