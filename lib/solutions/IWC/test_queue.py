b9b0441d16b8e26dcb06cd5f9fcb7f77c2656983
sbx_user1051 <sbx_user1051@169.254.66.45>
2026-02-03 18:55:14 +0000

    Tue Feb 03 18:55:14 UTC 2026

--- lib/solutions/IWC/test_queue.py
+++ lib/solutions/IWC/test_queue.py
@@ -2,44 +2,44 @@ from solutions.IWC.queue_solution_legacy import Queue
 from solutions.IWC.task_types import TaskDispatch, TaskSubmission
 from datetime import datetime
 
-# id = IWC_R2_S8_001, req = enqueue({"provider":"companies_house","timestamp":"2025-10-20 12:02:00","user_id":1}), resp = 1
-# id = IWC_R2_S8_002, req = enqueue({"provider":"id_verification","timestamp":"2025-10-20 12:04:00","user_id":1}), resp = 2
-# id = IWC_R2_S8_003, req = enqueue({"provider":"credit_check","timestamp":"2025-10-20 12:00:00","user_id":1}), resp = 4
-# id = IWC_R2_S8_004, req = dequeue(), resp = {"provider":"companies_house","user_id":1}
-# id = IWC_R2_S8_005, req = dequeue(), resp = {"provider":"credit_check","user_id":1}
-# id = IWC_R2_S8_006, req = dequeue(), resp = {"provider":"companies_house","user_id":1}
+# # id = IWC_R2_S8_001, req = enqueue({"provider":"companies_house","timestamp":"2025-10-20 12:02:00","user_id":1}), resp = 1
+# # id = IWC_R2_S8_002, req = enqueue({"provider":"id_verification","timestamp":"2025-10-20 12:04:00","user_id":1}), resp = 2
+# # id = IWC_R2_S8_003, req = enqueue({"provider":"credit_check","timestamp":"2025-10-20 12:00:00","user_id":1}), resp = 4
+# # id = IWC_R2_S8_004, req = dequeue(), resp = {"provider":"companies_house","user_id":1}
+# # id = IWC_R2_S8_005, req = dequeue(), resp = {"provider":"credit_check","user_id":1}
+# # id = IWC_R2_S8_006, req = dequeue(), resp = {"provider":"companies_house","user_id":1}
 
 
 
-def test_dedupe_credit_check():
-    task1 = TaskSubmission(
-        user_id=1,
-        provider="companies_house",
-        timestamp=datetime.strptime('2025-10-20 12:02:00', "%Y-%m-%d %H:%M:%S")
-    )
-    task2 = TaskSubmission(
-        user_id=1,
-        provider="id_verification",
-        timestamp=datetime.strptime('2025-10-20 12:04:00', "%Y-%m-%d %H:%M:%S")
-    )
-    task3 = TaskSubmission(
-        user_id=1,
-        provider="credit_check",
-        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
-    )
+# def test_dedupe_credit_check():
+#     task1 = TaskSubmission(
+#         user_id=1,
+#         provider="companies_house",
+#         timestamp=datetime.strptime('2025-10-20 12:02:00', "%Y-%m-%d %H:%M:%S")
+#     )
+#     task2 = TaskSubmission(
+#         user_id=1,
+#         provider="id_verification",
+#         timestamp=datetime.strptime('2025-10-20 12:04:00', "%Y-%m-%d %H:%M:%S")
+#     )
+#     task3 = TaskSubmission(
+#         user_id=1,
+#         provider="credit_check",
+#         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+#     )
 
-    queue = Queue()
-    queue.enqueue(task1)
-    assert len(queue._queue) == 1
-    queue.enqueue(task2)
-    assert len(queue._queue) == 2
-    queue.enqueue(task3)
-    assert len(queue._queue) == 3
+#     queue = Queue()
+#     queue.enqueue(task1)
+#     assert len(queue._queue) == 1
+#     queue.enqueue(task2)
+#     assert len(queue._queue) == 2
+#     queue.enqueue(task3)
+#     assert len(queue._queue) == 3
 
-    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
-    assert queue.dequeue() == TaskDispatch(provider="credit_check", user_id=1)
-    assert queue.dequeue() == TaskDispatch(provider="id_verification", user_id=1)
-    assert queue.dequeue() == None
+#     assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
+#     assert queue.dequeue() == TaskDispatch(provider="credit_check", user_id=1)
+#     assert queue.dequeue() == TaskDispatch(provider="id_verification", user_id=1)
+#     assert queue.dequeue() == None
 
 def test_dedupe():
     task1 = TaskSubmission(
@@ -70,85 +70,85 @@ def test_dedupe():
 
 
 
-def test_with_one_task():
-    task1 = TaskSubmission(
-        user_id=1,
-        provider="bank_statements",
-        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
-    )
+# def test_with_one_task():
+#     task1 = TaskSubmission(
+#         user_id=1,
+#         provider="bank_statements",
+#         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+#     )
  
-    queue = Queue()
-    queue.enqueue(task1)
+#     queue = Queue()
+#     queue.enqueue(task1)
 
-    assert queue._queue[0].timestamp == datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
-    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
-    assert queue.dequeue() == None
+#     assert queue._queue[0].timestamp == datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+#     assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
+#     assert queue.dequeue() == None
 
 
-def test_rule_of_three():
-    task1 = TaskSubmission(
-        user_id=1,
-        provider="companies_house",
-        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
-    )
-    task2 = TaskSubmission(
-        user_id=2,
-        provider="companies_house",
-        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
-    )
-    task3 = TaskSubmission(
-        user_id=1,
-        provider="id_verification",
-        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
-    )
-    task4 = TaskSubmission(
-        user_id=1,
-        provider="bank_statements",
-        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
-    )
+# def test_rule_of_three():
+#     task1 = TaskSubmission(
+#         user_id=1,
+#         provider="companies_house",
+#         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+#     )
+#     task2 = TaskSubmission(
+#         user_id=2,
+#         provider="companies_house",
+#         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+#     )
+#     task3 = TaskSubmission(
+#         user_id=1,
+#         provider="id_verification",
+#         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+#     )
+#     task4 = TaskSubmission(
+#         user_id=1,
+#         provider="bank_statements",
+#         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+#     )
 
-    queue = Queue()
-    queue.enqueue(task1)
-    queue.enqueue(task2)
-    queue.enqueue(task3)
-    queue.enqueue(task4)
+#     queue = Queue()
+#     queue.enqueue(task1)
+#     queue.enqueue(task2)
+#     queue.enqueue(task3)
+#     queue.enqueue(task4)
 
-    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
-    assert queue.dequeue() == TaskDispatch(provider="id_verification", user_id=1)
-    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
-    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=2)
+#     assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
+#     assert queue.dequeue() == TaskDispatch(provider="id_verification", user_id=1)
+#     assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
+#     assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=2)
 
-def test_timestamp_ordering():
-    task1 = TaskSubmission(
-        user_id=1,
-        provider="bank_statements",
-        timestamp=datetime.strptime('2025-10-20 12:05:00', "%Y-%m-%d %H:%M:%S")
-    )
-    task2 = TaskSubmission(
-        user_id=2,
-        provider="bank_statements",
-        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
-    )
+# def test_timestamp_ordering():
+#     task1 = TaskSubmission(
+#         user_id=1,
+#         provider="bank_statements",
+#         timestamp=datetime.strptime('2025-10-20 12:05:00', "%Y-%m-%d %H:%M:%S")
+#     )
+#     task2 = TaskSubmission(
+#         user_id=2,
+#         provider="bank_statements",
+#         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+#     )
 
-    queue = Queue()
-    queue.enqueue(task1)
-    queue.enqueue(task2)
+#     queue = Queue()
+#     queue.enqueue(task1)
+#     queue.enqueue(task2)
 
-    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=2)
-    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
+#     assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=2)
+#     assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
 
-def test_dependency_resolution():
-    task1 = TaskSubmission(
-        user_id=1,
-        provider="credit_check",
-        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
-    )
+# def test_dependency_resolution():
+#     task1 = TaskSubmission(
+#         user_id=1,
+#         provider="credit_check",
+#         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+#     )
 
-    queue = Queue()
-    queue.enqueue(task1)
+#     queue = Queue()
+#     queue.enqueue(task1)
 
-    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
-    assert queue.dequeue() == TaskDispatch(provider="credit_check", user_id=1)
+#     assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
+#     assert queue.dequeue() == TaskDispatch(provider="credit_check", user_id=1)
 
 
 
