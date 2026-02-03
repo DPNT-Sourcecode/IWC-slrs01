76eeff7738332de745b76a5f809959f38b679a33
sbx_user1051 <sbx_user1051@169.254.48.57>
2026-02-03 22:36:10 +0000

    Tue Feb 03 22:36:10 UTC 2026

--- lib/solutions/IWC/test_bank_statements.py
+++ lib/solutions/IWC/test_bank_statements.py
@@ -3,10 +3,44 @@ from solutions.IWC.task_types import TaskDispatch, TaskSubmission
 from datetime import datetime
 
 
+# id = IWC_R5_S12_001, req = enqueue({"provider":"companies_house","timestamp":"2025-10-20 12:07:00","user_id":1}), resp = 1
+# id = IWC_R5_S12_002, req = enqueue({"provider":"id_verification","timestamp":"2025-10-20 12:07:00","user_id":1}), resp = 2
+# id = IWC_R5_S12_003, req = enqueue({"provider":"bank_statements","timestamp":"2025-10-20 12:01:00","user_id":1}), resp = 3
+# id = IWC_R5_S12_004, req = enqueue({"provider":"credit_check","timestamp":"2025-10-20 12:00:00","user_id":1}), resp = 4
 
-# id = IWC_R5_S11_001, req = enqueue({"provider":"companies_house","timestamp":"2025-10-20 12:07:00","user_id":1}), resp = 1
-# id = IWC_R5_S11_002, req = enqueue({"provider":"bank_statements","timestamp":"2025-10-20 12:01:00","user_id":1}), resp = 2
-# id = IWC_R5_S11_003, req = enqueue({"provider":"companies_house","timestamp":"2025-10-20 12:00:00","user_id":2}), resp = 3
+def test_seond_oldest_bank_with_credit_check():
+    task1 = TaskSubmission(
+        user_id=1,
+        provider="companies_house",
+        timestamp=datetime.strptime('2025-10-20 12:07:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task2 = TaskSubmission(
+        user_id=1,
+        provider="id_verification",
+        timestamp=datetime.strptime('2025-10-20 12:07:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task3 = TaskSubmission(
+        user_id=1,
+        provider="bank_statements",
+        timestamp=datetime.strptime('2025-10-20 12:01:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task4 = TaskSubmission(
+        user_id=1,
+        provider="credit_check",
+        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+    )
+
+    queue = Queue()
+    queue.enqueue(task1)
+    queue.enqueue(task2)
+    queue.enqueue(task3)
+    queue.enqueue(task4)
+
+    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=2)
+    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
+    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
+    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
+
 
 
 def test_bank_second_oldest():
@@ -124,3 +158,4 @@ def test_time_sesative_bank_statments_with_later():
     assert queue.dequeue() == TaskDispatch(provider="id_verification", user_id=2)
     assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=3)
     assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=2)
+
