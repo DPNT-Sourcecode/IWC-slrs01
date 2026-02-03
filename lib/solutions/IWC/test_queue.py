dae80094c20ebd12c599d9729eb644a77a16be43
sbx_user1051 <sbx_user1051@169.254.66.45>
2026-02-03 19:24:02 +0000

    Tue Feb 03 19:24:02 UTC 2026

--- lib/solutions/IWC/test_queue.py
+++ lib/solutions/IWC/test_queue.py
@@ -9,6 +9,40 @@ from datetime import datetime
 # 5. Dequeue -> {"user_id": 2, "provider": "companies_house"}  
 # 6. Dequeue -> {"user_id": 1, "provider": "bank_statements"}  
 
+def test_bank_statements_deprio_with_rule_of_three():
+    task1 = TaskSubmission(
+        user_id=1,
+        provider="bank_statements",
+        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task2 = TaskSubmission(
+        user_id=1,
+        provider="id_verification",
+        timestamp=datetime.strptime('2025-10-20 12:01:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task3 = TaskSubmission(
+        user_id=2,
+        provider="companies_house",
+        timestamp=datetime.strptime('2025-10-20 12:02:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task4 = TaskSubmission(
+        user_id=1,
+        provider="companies_house",
+        timestamp=datetime.strptime('2025-10-20 12:03:00', "%Y-%m-%d %H:%M:%S")
+    )
+
+    queue = Queue()
+    queue.enqueue(task1)
+    queue.enqueue(task2)
+    queue.enqueue(task3)
+    queue.enqueue(task4)
+
+    assert queue.dequeue() == TaskDispatch(provider="id_verification", user_id=1)
+    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
+    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
+    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=2)
+    assert queue.dequeue() == None
+
 
 def test_bank_statements_deprio():
     task1 = TaskSubmission(
@@ -178,5 +212,6 @@ def test_dependency_resolution():
 
     assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
     assert queue.dequeue() == TaskDispatch(provider="credit_check", user_id=1)
+
 
 
