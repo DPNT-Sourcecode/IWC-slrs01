74457936110dbd3ea7c2e2d7a8d9994020ccc0ac
sbx_user1051 <sbx_user1051@169.254.48.57>
2026-02-03 21:50:30 +0000

    Tue Feb 03 21:50:30 UTC 2026

--- lib/solutions/IWC/test_queue.py
+++ lib/solutions/IWC/test_queue.py
@@ -38,6 +38,25 @@ def test_age_with_no_task():
     queue = Queue()
     assert queue.age == 0
 
+def test_age_as_strings():
+    task1 = TaskSubmission(
+        user_id=1,
+        provider="id_verification",
+        timestamp='2025-10-20 12:00:00'
+    )
+    task2 = TaskSubmission(
+        user_id=1,
+        provider="bank_statements",
+        timestamp='2025-10-20 12:05:00'
+    )
+
+    queue = Queue()
+    queue.enqueue(task1)
+    queue.enqueue(task2)
+
+    assert queue.age == 300
+
+
 
 def test_bank_statement_deprio_with_two_user():
     task1 = TaskSubmission(
@@ -270,4 +289,5 @@ def test_dependency_resolution():
 
     assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
     assert queue.dequeue() == TaskDispatch(provider="credit_check", user_id=1)
+
 
