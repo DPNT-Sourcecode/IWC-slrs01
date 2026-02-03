fcdd3caac712d16dace908d6cc33f4dd990cc02b
sbx_user1051 <sbx_user1051@169.254.66.45>
2026-02-03 18:02:50 +0000

    Tue Feb 03 18:02:50 UTC 2026

--- lib/solutions/IWC/test_queue.py
+++ lib/solutions/IWC/test_queue.py
@@ -2,18 +2,12 @@ from solutions.IWC.queue_solution_legacy import Queue
 from solutions.IWC.task_types import TaskDispatch, TaskSubmission
 from datetime import datetime
 
-# 1. Enqueue: user_id=1, provider="companies_house",   timestamp='2025-10-20 12:00:00'  -> 1 (queue size)  
-# 2. Enqueue: user_id=2, provider="bank_statements",   timestamp='2025-10-20 12:00:00'  -> 2 (queue size)  
-# 3. Enqueue: user_id=1, provider="id_verification",   timestamp='2025-10-20 12:00:00'  -> 3 (queue size)  
-# 4. Enqueue: user_id=1, provider="bank_statements",   timestamp='2025-10-20 12:00:00'  -> 4 (queue size)
-
 def test_rule_of_three():
     task1 = TaskSubmission(
         user_id=1,
         provider="companies_house",
         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
     )
-
     task2 = TaskSubmission(
         user_id=2,
         provider="companies_house",
@@ -31,7 +25,6 @@ def test_rule_of_three():
     )
 
     queue = Queue()
-
     queue.enqueue(task1)
     queue.enqueue(task2)
     queue.enqueue(task3)
@@ -41,3 +34,34 @@ def test_rule_of_three():
     assert queue.dequeue() == TaskDispatch(provider="id_verification", user_id=1)
     assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
     assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=2)
+
+
+# Example #2 - Timestamp Ordering:
+# --------
+# The following operations show how the order of tasks is determined by their timestamp.
+
+# 1. Enqueue: user_id=1, provider="bank_statements", timestamp='2025-10-20 12:05:00'  -> 1 (queue size)
+# 2. Enqueue: user_id=2, provider="bank_statements", timestamp='2025-10-20 12:00:00'  -> 2 (queue size)
+# 3. Dequeue -> {"user_id": 2, "provider": "bank_statements"}  
+# 4. Dequeue -> {"user_id": 1, "provider": "bank_statements"}  
+
+def test_timestamp_ordering():
+    task1 = TaskSubmission(
+        user_id=1,
+        provider="bank_statements",
+        timestamp=datetime.strptime('2025-10-20 12:05:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task2 = TaskSubmission(
+        user_id=2,
+        provider="bank_statements",
+        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+    )
+
+    queue = Queue()
+    queue.enqueue(task1)
+    queue.enqueue(task2)
+
+    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=2)
+    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
+
+
