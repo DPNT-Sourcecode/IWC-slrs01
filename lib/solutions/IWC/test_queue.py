d8719c9712b470700389edb1ad71fca526a1b904
sbx_user1051 <sbx_user1051@169.254.66.45>
2026-02-03 17:56:50 +0000

    Tue Feb 03 17:56:50 UTC 2026

--- lib/solutions/IWC/test_queue.py
+++ lib/solutions/IWC/test_queue.py
@@ -13,3 +13,31 @@ def test_rule_of_three():
         provider="companies_house",
         timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
     )
+
+    task2 = TaskSubmission(
+        user_id=2,
+        provider="companies_house",
+        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task3 = TaskSubmission(
+        user_id=1,
+        provider="id_verification",
+        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+    )
+    task4 = TaskSubmission(
+        user_id=1,
+        provider="bank_statements",
+        timestamp=datetime.strptime('2025-10-20 12:00:00', "%Y-%m-%d %H:%M:%S")
+    )
+
+    queue = Queue()
+
+    queue.enqueue(task1)
+    queue.enqueue(task2)
+    queue.enqueue(task3)
+    queue.enqueue(task4)
+
+    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=1)
+    assert queue.dequeue() == TaskDispatch(provider="id_verification", user_id=1)
+    assert queue.dequeue() == TaskDispatch(provider="bank_statements", user_id=1)
+    assert queue.dequeue() == TaskDispatch(provider="companies_house", user_id=2)
