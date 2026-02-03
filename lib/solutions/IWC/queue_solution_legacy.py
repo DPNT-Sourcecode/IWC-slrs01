565568e7f5736b6a50f446b38e797c676c21f29f
sbx_user1051 <sbx_user1051@169.254.66.45>
2026-02-03 18:30:35 +0000

    Tue Feb 03 18:30:35 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -96,7 +96,8 @@ class Queue:
             if task.provider == item.provider and task.user_id == item.user_id:
                 if task.timestamp >= item.timestamp:
                     self._queue.pop(i)
-                return self.size
+                else:
+                    return self.size
                 
         tasks = [*self._collect_dependencies(item), item]
 
@@ -249,6 +250,7 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
+
 
 
 
